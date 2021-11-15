39144607a1496ca497a84e953e3ddb2e71fbf409
ggBudet <mymrin.gm@gmail.com>
2021-11-15 22:41:31 +0300

    other window

---
+++ Accountant.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab
+{
+    /// <summary>
+    /// Логика взаимодействия для Accountant.xaml
+    /// </summary>
+    public partial class Accountant : Window
+    {
+        public Accountant()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Laborant.xaml.cs
+++ Laborant.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,17 +8,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace lab
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Laborant.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Laborant : Window
     {
-        public MainWindow()
+        public Laborant()
         {
             InitializeComponent();
         }
